f74e261442312e4424fa52bbfcd570e51f157cd0
Julie A. Berry <Unf17t3Rd@codestardev.com>
2015-03-04 21:44:08 -0800

    create ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,62 @@
+# variable my name is open print Julie a. Berry close print
+my_name = 'Julie a. Berry'
+
+# variable my_age is 51
+my_age = 51 # born in 1963
+
+# variable my_height is 64
+my_height = 64 # inches
+
+# variable my_weight is 166
+my_weight = 166 # guessing
+
+# variable my_eyes is open print Blue close print
+my_eyes = 'Blue'
+
+# variable my_teeth is open print White close print
+my_teeth = 'White'
+
+# variable my_hair is open print Blonde close print
+my_hair = 'Blonde'
+
+# puts open string Let's talk about call variable my_name close variable. close string
+puts "Let's talk about #{my_name}."
+
+# puts open string She's call variable my_height close variable inchces tall. close string
+puts "She's #{my_height} inches tall."
+
+# puts open string She's call variable my_weight close variable pounds heavy. close string
+puts "She's #{my_weight} pounds heavy."
+
+# puts open string Actually that's pretty heavy. close string
+puts "Actually that's pretty heavy."
+
+# puts open string She's got call variable my_eyes close variable and call variable my_hair close variable hair. close string
+puts "She's got #{my_eyes} and #{my_hair} hair."
+
+# puts open string Her teeth are usually call variable my_teeth close variable depending on the wine. close string
+puts "Her teeth are usually #{my_teeth} depending on the wine."
+
+# this line is tricky so I'm trying to get it right
+# puts open string If I add call variable may_age close variable, call variable my_height close variable, and call veriable my_weight close variable I get call variable my_age plus my_height plus my_weight close variable. close string
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+
+puts "EXTRA CREDIT 1 \nChange all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them."
+# variable my name is open print Julie a. Berry close print
+name = 'Julie a. Berry'
+age = 51
+height = 64 # inches
+weight = 166 # guessing
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Blonde'
+puts "Let's talk about #{name}."
+puts "She's #{height} inches tall."
+puts "She's #{weight} pounds heavy."
+puts "Actually that's pretty heavy."
+puts "She's got #{eyes} and #{hair} hair."
+puts "Her teeth are usually #{teeth} depending on the wine."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+puts "EXTRA CREDIT 2 \nTry to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby."
+# ???

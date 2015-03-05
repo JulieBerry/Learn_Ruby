6eff286fcbb4a92ed0247314eb7c150bf35ccc9f
Julie A. Berry <Unf17t3Rd@codestardev.com>
2015-03-04 21:41:36 -0800

    create ex3.rb

---
+++ ex3.rb
@@ -0,0 +1,96 @@
+# Counting my chickens
+# put string open string I will now count my chickens: close string
+puts "I will now count my chickens:"
+
+# Uses the comma operator to output multiple values
+# put string open string Hens call variable 25 plus 30 divided by 6 close variable close string
+puts "Hens #{25 + 30 / 6}"
+
+# Uses the comma operator to output multiple values
+# put string open string call variable 100 minus 25 times 3 percent 4 close variable close string
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Prints a string
+# put string open string Mow I will count the eggs: close string
+puts "Now I will count the eggs:"
+
+# Computes a bunch of math and prints the results
+# put string 3 plus 2 plus 1 minus 5 plus 4 percent 2 minus 1 divided by 4 plus 6
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# prints a string
+# put string open string Is is true thave 3 plus 2 is less than 5 minus 7? close string
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Prints the results of math inequality
+# put string 3 plus 2 is less than 5 minus 7
+puts 3 + 2 < 5 - 7
+
+# prints a string followed by the value of a math operation 
+# put string open string What is 3 plus two? get math 3 + 2 close math close string
+puts "What is 3 + 2? #{3 + 2}"
+
+# prints a string followed by the value of a math operation 
+# put string open string What is 5 minus 7? get math  5 - 7 close math close string
+puts "What is 5 - 7? #{5 - 7}"
+
+# prints a string
+# put string open string Oh, that's why it's false. close string
+puts "Oh, that's why it's false."
+
+# prints a string
+# put string open string How about some more. close string
+puts "How about some more."
+
+# prints a string followed by the value of a math operation 
+# put string open string Is it greater? get math 5 > -2 close math close string
+puts "Is it greater? #{5 > -2}"
+
+# prints a string followed by the value of a math operation 
+# put string open string Is is greater or equal? get math 5 >= -2 close math close string
+puts "Is it greater or equal? #{5 >= -2}"
+
+# prints a string followed by the value of a math operation 
+# put string opem string Is it less or equal? get math 5 <= -2 close math close string
+puts "Is it less or equal? #{5 <= -2}"
+
+# EXTRA CREDIT 1 - Above each line, use the # to write a comment to yourself explaining what the line does.
+
+# EXTRA CREDIT 2 - Remember in Exercise 0 when you started Ruby? Start Ruby this way again and using the math operators, use Ruby as a calculator.
+# [ ?? ]
+
+# EXTRA CREDIT 3 - Find something you need to calculate and write a new .rb file that does it.
+# [Please see file ex3_1.rb]
+
+puts "EXTRA CREDIT 4\nNotice the math seems 'wrong'? There are no fractions, only whole numbers. You need to use a 'floating point' number, which is a number with a decimal point, as in 10.5, or 0.89, or even 3.0."
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "EXTRA CREDIT 5\nRewrite ex3.rb to use floating point numbers so it's more accurate. 20.0 is floating point."
+
+puts "\nI will now count my chickens:"
+
+puts "Hens #{25.0 + 30.0 / 6.0}"
+
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+puts "Now I will count the eggs:"
+
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5.0 > -2.0}"
+
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+
+puts "Is it less or equal? #{5.0 <= -2.0}"

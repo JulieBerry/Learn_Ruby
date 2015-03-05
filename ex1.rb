263d1c81b713d0da15ef5fc029b786684e4cddab
Julie A. Berry <Unf17t3Rd@codestardev.com>
2015-03-04 21:39:23 -0800

    add new file

---
+++ ex1.rb
@@ -0,0 +1,28 @@
+# put string open string Hello world! close string
+puts "Hello World!"
+
+# put string open string Hello Again close string
+puts "Hello Again"
+
+# put string open string I like typing this. close string
+puts "I like typing this."
+
+# put string open string This is fun. close string 
+puts "This is fun."
+
+# put string open string Yay! Printing. close string
+puts "Yay! Printing."
+
+# put string open string I'd much rather you 'not'. close string
+puts "I'd much rather you 'not'."
+
+# put string open string I "said" do not touch this. close string
+puts 'I "said" do not touch this.'
+
+# Extra Credit 1 - Make your script print another line.
+puts "\nThis is my new line"
+
+# Extra Credit 2 - Make your script print only one of the lines.
+
+# Extra Credit 3 - Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+# Answer: It changes the string to a comment and will not print anything to the right of the octothorpe

b71adcb07157434c688ae78f8122489ed3de81e1
Julie A. Berry <Unf17t3Rd@codestardev.com>
2015-03-04 21:40:30 -0800

    create ex2.rb

---
+++ ex2.rb
@@ -0,0 +1,19 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+
+# put string open string I could have code like this. close string
+puts "I could have code like this." # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# puts "This won't run."
+
+# put string open string This will run. close string
+puts "This will run."
+
+# EXTRA CREDIT 1 - Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or pound character).
+
+# EXTRA CREDIT 2 - Take your ex2.rb file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed.
+
+# EXTRA CREDIT 3 - Did you find more mistakes? Fix them. [no mistakes so far] 
+
+# EXTRA CREDIT 4 - Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix them.
